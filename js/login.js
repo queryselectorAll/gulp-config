06313762f38cb6d6578131ed90e8073f5c710398
qinyhquery <15927700473@163.com>
2018-01-12 23:21:51 +0800

    添加es6转码配置

--- js/login.js
+++ js/login.js
@@ -1,34 +1,49 @@
 /**
- * 登录页面
+ * 登录
  */
+var username =  document.querySelector('#username'),
+    password =  document.querySelector('#password');
+    
+username.addEventListener('focus',function(){
+    this.parentNode.children[0].classList.add('focus-bgc');
+});
+username.addEventListener('blur',function(){
+    this.parentNode.children[0].classList.remove('focus-bgc');
+});
+password.addEventListener('focus',function(){
+    this.parentNode.children[0].classList.add('focus-bgc');
+});
+password.addEventListener('blur',function(){
+    this.parentNode.children[0].classList.remove('focus-bgc');
+});
 
-$('#login_form').on('submit', function () {
+document.querySelector('.login-form').addEventListener('submit',function(e){
+    var usernameCheck = username.value === '' || null;
+    var passwordCheck = password.value === '' || null;
+    if(usernameCheck || passwordCheck){
+        document.querySelector('.msg-error').style.display = 'block';
+        document.querySelector('.msg-content').innerHTML = '请输入账户名或密码';
+    }else{
+        var usernameParam = username.value;
+        var passwordParam = password.value;
 
-    var formData = $(this).serialize();
-    var custName = $('#custName').val();
-    var password = $('#password').val();
-    $.ajax({
-        url: baseUrl + 'lcyj-common-wap/app/appLogin',
-        // url: ' /lcyjapp/appLogin',
-        type: 'post',
-        data: formData,
-        success: function (data) {
-            console.log(data)
-            if (data.status === 'success') {
-                mui.toast(data.msg);
-                // window.location.href = './carinfo.html';
-                var webview = mui.openWindow({
-                    url: './carinfo.html',
-                    id: 'carinfo',
-                    show: {
-                        aniShow: 'pop-in',//页面显示动画，默认为”slide-in-right“；
-                    },
-					waiting: {
-						autoShow: false
-					}
-                });
+        var xhr = new XMLHttpRequest();
+        xhr.open('post','../php/login.php',true);
+        xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+        var pamam = 'username=' + usernameParam + '&password=' + passwordParam;
+        xhr.send(pamam);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr.status == 200){
+                var data = xhr.responseText;
+                console.log(data);
+                document.querySelector('.msg-content').innerHTML = data;
+                document.querySelector('.msg-error').style.display = 'block';
+                if(data === '登录成功'){
+                    var location = window.location;
+                    location.href = '../index.html';
+                }
             }
-        }
-    });
-    return false;
+        };
+    }
+    e.preventDefault();
 });
